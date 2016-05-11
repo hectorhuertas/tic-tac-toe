91223f20b14a177f29239e0f256e111bdd1b8084
Hector Huertas <hectorhuertas@gmail.com>
2016-05-11 20:56:41 +0200

    Improve game creation test

--- test/integration/game_creation_test.rb
+++ test/integration/game_creation_test.rb
@@ -5,6 +5,9 @@ class GameCreationTest < ActionDispatch::IntegrationTest
     get '/api/v1/games/new?size=3'
 
     json = JSON.parse(response.body)
-    assert_equal json['game']['size'], 3
+    game = Game.last
+
+    assert_equal json['game']['size'], game.size
+    assert_equal json['game']['id'], game.id
   end
 end
